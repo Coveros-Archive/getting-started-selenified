062efbaa0e4e07b22b88d2806666aa550de605cc
Matt Grasberger <grasbergerm@gmail.com>
2017-08-23 16:29:02 -0400

    update tests for selenified 3.0

--- src/test/java/coveros/getting_started/SampleTests.java
+++ src/test/java/coveros/getting_started/SampleTests.java
@@ -3,26 +3,26 @@ package coveros.getting_started;
 import java.io.IOException;
 
 import org.openqa.selenium.Keys;
+import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.coveros.selenified.output.Assert;
-import com.coveros.selenified.selenium.Action;
-import com.coveros.selenified.selenium.Selenium.Locator;
-import com.coveros.selenified.tools.TestBase;
+import com.coveros.selenified.Locator;
+import com.coveros.selenified.Selenified;
+import com.coveros.selenified.application.App;
 
-public class SampleTests extends TestBase {
+public class SampleTests extends Selenified {
 
 	@BeforeClass(alwaysRun = true)
-	public void beforeClass() throws IOException {
+	public void beforeClass(ITestContext test) {
 		// set the base URL for the tests here
-		setTestSite("https://www.google.com");
+		setTestSite(this, test, "https://www.google.com");
 		// set the author of the tests here
-		setAuthor("Matt Grasberger\n<br/>matthew.grasberger@coveros.com");
+		setAuthor(this, test, "Matt Grasberger\n<br/>matthew.grasberger@coveros.com");
 		// set the version of the tests or of the software, possibly with a
 		// dynamic check
-		setVersion("0.0.1");
+		setVersion(this, test, "0.0.1");
 	}
 
 	@DataProvider(name = "google search terms", parallel = true)
@@ -32,10 +32,10 @@ public class SampleTests extends TestBase {
 
 	@Test(groups = { "sample" }, description = "A sample test to check a title")
 	public void sampleTest() throws IOException {
-		// use this object to verify the page looks as expected
-		Assert asserts = this.asserts.get();
+	    // grab our main app object
+        App app = this.apps.get();
 		// perform some actions
-		asserts.compareTitle("Google");
+		app.azzert().titleEquals("Google");
 		// verify no issues
 		finish();
 	}
@@ -43,15 +43,13 @@ public class SampleTests extends TestBase {
 	@Test(dataProvider = "google search terms", groups = { "sample" },
 			description = "A sample test using a data provider to perform searches")
 	public void sampleTestWDataProvider(String searchTerm) throws Exception {
-		// use this object to manipulate the page
-		Action actions = this.actions.get();
-		// use this object to verify the page looks as expected
-		Assert asserts = this.asserts.get();
+	 // grab our main app object
+        App app = this.apps.get();
 		// perform some actions
-		actions.type(Locator.NAME, "q", searchTerm);
-		actions.type(Locator.NAME, "q", Keys.ENTER);
-		actions.waitForElementDisplayed(Locator.ID, "resultStats");
-		asserts.compareTitle(searchTerm + " - Google Search");
+		app.newElement(Locator.NAME, "q").type(searchTerm);
+		app.newElement(Locator.NAME, "q").type(Keys.ENTER);
+		app.newElement(Locator.ID, "resultStats").waitFor().displayed();
+		app.azzert().titleEquals(searchTerm + " - Google Search");
 		// verify no issues
 		finish();
 	}
