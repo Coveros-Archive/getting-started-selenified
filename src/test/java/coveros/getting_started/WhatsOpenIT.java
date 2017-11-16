b258416ceee2f0fa05fc8a5b738534c730e5a799
Matt Grasberger <grasbergerm@gmail.com>
2017-11-15 20:50:21 -0500

    initial commit

--- src/test/java/coveros/getting_started/WhatsOpenIT.java
+++ src/test/java/coveros/getting_started/WhatsOpenIT.java
@@ -1,8 +1,5 @@
 package coveros.getting_started;
 
-import java.io.IOException;
-
-import org.openqa.selenium.Keys;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -12,12 +9,12 @@ import com.coveros.selenified.Locator;
 import com.coveros.selenified.Selenified;
 import com.coveros.selenified.application.App;
 
-public class SampleTests extends Selenified {
+public class WhatsOpenIT extends Selenified{
 
     @BeforeClass(alwaysRun = true)
     public void beforeClass(ITestContext test) {
         // set the base URL for the tests here
-        setTestSite(this, test, "https://www.google.com");
+        setTestSite(this, test, "https://whatsopen.gmu.edu/");
         // set the author of the tests here
         setAuthor(this, test, "Matt Grasberger\n<br/>matthew.grasberger@coveros.com");
         // set the version of the tests or of the software, possibly with a
@@ -25,32 +22,30 @@ public class SampleTests extends Selenified {
         setVersion(this, test, "0.0.1");
     }
 
-    @DataProvider(name = "google search terms", parallel = true)
+    @DataProvider(name = "whats open locations", parallel = true)
     public Object[][] DataSetOptions() {
-        return new Object[][] { new Object[] { "python" }, new Object[] { "perl" }, new Object[] { "bash" }, };
+        return new Object[][] { new Object[] { "127","Chick-Fil-A" }, new Object[] { "1450", "Blaze Pizza" }, new Object[] { "199", "One Stop Patriot Shop" }, };
     }
 
-    @Test(groups = { "sample" }, description = "A sample test to check a title")
-    public void sampleTest() throws IOException {
+    @Test(groups = { "sample" }, description = "A sample test to check the title")
+    public void sampleTest() {
         // grab our main app object
         App app = this.apps.get();
         // perform some actions
-        app.azzert().titleEquals("Google");
+        app.azzert().titleEquals("What's Open - George Mason University");
+        // verify no issues
+        finish();
+    }
+    
+    @Test(dataProvider = "whats open locations", groups = { "sample" }, description = "A sample test to check a few different locations")
+    public void sampleTestClickChickFilA(String buttonID, String locationName) {
+        // grab our main app object
+        App app = this.apps.get();
+        // perform some actions
+        app.newElement(Locator.ID, buttonID).click();
+        app.newElement(Locator.ID, "info-name").assertEquals().text(locationName);
         // verify no issues
         finish();
     }
 
-    @Test(dataProvider = "google search terms", groups = { "sample" },
-            description = "A sample test using a data provider to perform searches")
-    public void sampleTestWDataProvider(String searchTerm) throws Exception {
-        // grab our main app object
-        App app = this.apps.get();
-        // perform some actions
-        app.newElement(Locator.NAME, "q").type(searchTerm);
-        app.newElement(Locator.NAME, "q").type(Keys.ENTER);
-        app.newElement(Locator.ID, "resultStats").waitFor().displayed();
-        app.azzert().titleEquals(searchTerm + " - Google Search");
-        // verify no issues
-        finish();
-    }
 }
